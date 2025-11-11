16bb61e773246edd576c891ca2cdd7344ebf57fa
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-11-11 15:55:07 +0100

    update file endings

--- dlr_118_to_gate.launch.py
+++ dlr_118_to_gate.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
             controllable=True,
             v2x_id=0,
             vehicle_id=111,
-            controller=1,
+            controller=2,
             debug=False
         )
     ])
